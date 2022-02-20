0d287f288220a38e771b27f7fa19e1bf2014f7a3
Yoshio Mori <yoshio.mori@hotmail.com>
2022-02-20 06:58:02 -0300

    Refatorando

--- view.py
+++ view.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter
 from tkinter import ttk
 
 import onlinereader
@@ -7,31 +7,29 @@ c = onlinereader.read('last7days')
 
 
 def calc_kw(d, h):
-    return (
-        dict() if c[h][d] == 0
-        else dict(background='red') if c[h][d] < 900
-        else dict(background='green') if c[h][d] < 1800
-        else dict(background='cyan') if c[h][d] < 2700
-        else dict(background='blue')
-    )
+    if c[h][d] == 0:
+        return dict()
+    elif c[h][d] < 900:
+        return dict(background='red')
+    elif c[h][d] < 1800:
+        return dict(background='green')
+    elif c[h][d] < 2700:
+        return dict(background='cyan')
+    else:
+        return dict(background='blue')
 
 
-root = Tk()
+root = tkinter.Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="Dom").grid(column=0, row=0)
-ttk.Label(frm, text="Seg").grid(column=1, row=0)
-ttk.Label(frm, text="Ter").grid(column=2, row=0)
-ttk.Label(frm, text="Qua").grid(column=3, row=0)
-ttk.Label(frm, text="Qui").grid(column=4, row=0)
-ttk.Label(frm, text="Sex").grid(column=5, row=0)
-ttk.Label(frm, text="Sáb").grid(column=6, row=0)
-for h in range(24):
-    ttk.Label(frm, text=f"{h:02}:00", **calc_kw(0, h)).grid(column=0, row=h+1)
-    ttk.Label(frm, text=f"{h:02}:00", **calc_kw(1, h)).grid(column=1, row=h+1)
-    ttk.Label(frm, text=f"{h:02}:00", **calc_kw(2, h)).grid(column=2, row=h+1)
-    ttk.Label(frm, text=f"{h:02}:00", **calc_kw(3, h)).grid(column=3, row=h+1)
-    ttk.Label(frm, text=f"{h:02}:00", **calc_kw(4, h)).grid(column=4, row=h+1)
-    ttk.Label(frm, text=f"{h:02}:00", **calc_kw(5, h)).grid(column=5, row=h+1)
-    ttk.Label(frm, text=f"{h:02}:00", **calc_kw(6, h)).grid(column=6, row=h+1)
+week_name_list = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"]
+column_count = 0
+for week_name in week_name_list:
+    ttk.Label(frm, text=week_name).grid(column=column_count, row=0)
+    column_count += 1
+for hour in range(24):
+    column_count = 0
+    while column_count < 7:
+        ttk.Label(frm, text=f"{hour:02}:00", **calc_kw(column_count, hour)).grid(column=column_count, row=hour+1)
+        column_count += 1
 root.mainloop()
